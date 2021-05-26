cd85397a50ef00684ce709ec62edbcc5a0548872
jsj6390 <81224989+jsj6390@users.noreply.github.com>
2021-05-26 15:18:19 +0900

    on/off버튼

--- main.ts
+++ main.ts
@@ -1,27 +1,31 @@
+input.onButtonPressed(Button.A, function () {
+    if (전원 == 0) {
+        전원 = 1
+    } else {
+        전원 = 0
+    }
+})
 function 라인트레이서 () {
-    if (RingbitCar.tracking(RingbitCar.TrackingStateType.Tracking_State_2)) {
+    if (RingbitCar.tracking(RingbitCar.TrackingStateType.Tracking_State_2) && 전원 == 1) {
         RingbitCar.freestyle(40, 0)
-        basic.pause(100)
     }
-    if (RingbitCar.tracking(RingbitCar.TrackingStateType.Tracking_State_1)) {
+    if (RingbitCar.tracking(RingbitCar.TrackingStateType.Tracking_State_1) && 전원 == 1) {
         RingbitCar.freestyle(0, 40)
-        basic.pause(100)
     }
-    if (RingbitCar.tracking(RingbitCar.TrackingStateType.Tracking_State_0)) {
+    if (RingbitCar.tracking(RingbitCar.TrackingStateType.Tracking_State_0) && 전원 == 1) {
         RingbitCar.freestyle(50, 50)
-        basic.pause(100)
     }
 }
 function 위치찾기 () {
-    if (RingbitCar.tracking(RingbitCar.TrackingStateType.Tracking_State_3)) {
-        RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 10)
+    if (RingbitCar.tracking(RingbitCar.TrackingStateType.Tracking_State_3) && 전원 == 1) {
         RingbitCar.freestyle(10, 10)
+        RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 10)
     }
 }
+let 전원 = 0
 RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
-RingbitCar.forward()
+전원 = 0
 basic.forever(function () {
     라인트레이서()
     위치찾기()
-    RingbitCar.freestyle(70, 70)
 })
